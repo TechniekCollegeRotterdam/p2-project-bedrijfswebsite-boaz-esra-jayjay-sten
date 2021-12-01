5d36df576b723532b21e1bec9277bb37613972a4
9017412@student.zadkine.nl <9017412@student.zadkine.nl>
2021-12-01 13:10:24 +0100

    contact.php changes

--- contact.php
+++ contact.php
@@ -18,14 +18,17 @@
     </header>
 
     <main class="main-contact">
+        <section class="con-con">
+            <section class="contact-items">
+            <h1>Contact us!</h1>
+            <P>Phone: +31 88 123 567 875</P>
+            <p>Email: Saturnus@helpdesk.com</p>
+            <p>Fax: 457-878-9056</p>
+            </section>
 
 
-
-
-
-
-
-
+        </secti>
+     
 
 
 
