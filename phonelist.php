27cb572c6031fe9a9380d50829320f55423a46f9
Boaz Arsav <9004955@student.tcrmbo.nl>
2021-12-08 14:35:41 +0100

    phonelist bijgerwerkt

---
+++ phonelist.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Games</title>
+  <link id="pagestyle" rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+</head>
+
+<body>
+
+  <?php include 'nav.html'?>
+
+  <main class="product-main">
+
+
+    <!--Allemaal Producten-->
+
+    <section class="sectionprod">
+      <article class="articleprod">
+        <h2><a href="#">Samsung Galaxy S9</a></h2>
+        <img src="phoneimg/s9.png" width="180px" height="280px" alt="Samsung s9">
+        <p>Michael, een ex-bankovervaller met pensioen, leeft het goede leven in de villawijk Rockford Hills. Zijn
+          vrouw
+          geeft heel de dag geld uit aan kleren, dus wordt het tijd dat hij zijn oude leventje weer op pakt voordat het
+          geld op is.<br><br> Samen met zijn voormalige partner Trevor en de jonge Franklin wordt Michael in het
+          criminele
+          circuit van Los Santos gezogen, op zoek naar the almighty American dollar.</p>
+        <p class="rating"><b>Pegi: 18+</b> <b>Systeem: PS4 / XBOX ONE / PC</b></p>
+      </article>
+    </section>
+
+
+    <section class="sectionprod">
+      <article class="articleprod">
+        <h2><a href="#">Samsung Galaxy S9+</a></h2>
+        <img src="phoneimg/s9+.jpg" width="180px" height="280px" alt="S9+">
+        <p>Strijd op alle fronten: voer luchtgevechten boven de Grote Oceaan, ga parachutespringen boven Frankrijk,
+          verdedig Stalingrad met de precisie van een sluipschutter en beschiet oprukkende troepen in
+          Noord-Afrika.<br><br> De
+          Call of Duty®-franchise keert terug met Call of Duty®: Vanguard, ontwikkeld door Sledgehammer Games, dat
+          spelers meevoert in meeslepende WO2-gevechten op een ongekende wereldwijde schaal.</p>
+        <p class="rating"><b>Pegi: 18+</b> <b>Systeem: PS4 & PS5 / XBOX ONE & Series X / PC</b></p>
+      </article>
+    </section>
+
+    <section class="sectionprod">
+      <article class="articleprod">
+        <h2><a href="#">Samsung Galaxy S20</a></h2>
+        <img src="phoneimg/s20.png" width="180px" height="280px" alt="Samsung Galaxy S20">
+        <p>Speel de genrebepalende klassiekers van de originele Grand Theft Auto Trilogy: Grand Theft Auto III, Grand
+          Theft Auto: Vice City en Grand Theft Auto: San Andreas.<br><br> Geüpdatet voor een nieuwe generatie, nu met
+          algemene
+          verbeteringen, waaronder briljante nieuwe verlichting en omgevingsupgrades, met texturen met hoge resolutie,
+          grotere kijkafstanden, Grand Theft Auto V-stijl besturing en targeting, en nog veel meer, waardoor deze
+          geliefde werelden tot leven komen met geheel nieuwe detailniveaus.</p>
+        <p class="rating"><b>Pegi: 18+</b> <b>Systeem: PS4 / XBOX ONE / PC / Switch</b></p>
+      </article>
+    </section>
+
+    <section class="sectionprod">
+      <article class="articleprod">
+        <h2><a href="#">Iphone X</a></h2>
+        <img src="phoneimg/applex.jpg" width="200px" height="280px" alt="APPLE X">
+        <p>Trek het masker van Joker aan en word lid van de Phantom Thieves of Hearts. Breek los van de ketens van de
+          moderne samenleving en organiseer overvallen om de geesten van de gecorrumpeerden te infiltreren en hen hun
+          wegen te laten veranderen!<br><br>Maak je klaar om je vaardigheden te versterken in de metaverse en in je
+          dagelijkse leven, in een nieuw semester op Shujin Academy. Persona 5 Royal presenteert een unieke visuele
+          stijl en ook bekroonde componist Shoji Meguro is terug met een geheel nieuwe soundtrack. Verken Tokyo,
+          ontgrendel nieuwe Personas, pas je eigen Thieves Den aan, ontdek een volledig nieuwe verhaallijn,
+          tussenfilmpjes, alternatieve eindes en meer!</p>
+        <p class="rating"><b>Pegi: 16+</b> <b>Systeem: PS4</b></p>
+      </article>
+    </section>
+
+    <section class="sectionprod">
+      <article class="articleprod">
+        <h2><a href="#">Iphone 12</a></h2>
+        <img src="phoneimg/apple12.jpg" width="200px" height="280px" alt="Iphone 12">
+        <p><b>Inport Product!</b> Geniet lekker thuis op de bank van de klassieke arcaderitmegame uit Japan! Speel met
+          je eigen drum, volg de
+          beat in allerlei nieuwe nummers en daag je vrienden uit om te bewijzen wie het beste in taiko is!<br><br>
+          •SPEEL MET JE VRIENDEN: Speel samen in coöp - of versus - modus of online met ghost van andere mensen -
+          gegevens van mensen over de wereld voor de hoogste score.<br>
+          •RANKED MODE: Maak online verbinding en neem het op tegen de ghost-gegevens van Taiko-spelers van over de hele
+          wereld om te zien wie het allerbeste is!<br>
+          •VEEL PERSONAGES BESCHIKBAAR: Speel Guest Session met personages als Hatsune Miku, Heihachi Mishima en vele
+          andere!<br>
+          •GENIET VAN ICONISCHE NUMMERS: Meer dan 70 nummers uit Dragon Ball Super, Zootopia. Frozen en nog veel
+          meer!<br>
+          •ONTGRENDELBARE CONTENT: Speel de game en ontgrendel kostuums voor Don-chan, de mascotte van de Taiko-serie,
+          geluidseffecten en meer!</p>
+        <p class="rating"><b>Pegi: 16+</b> <b>Systeem: PS4</b> <b>Regio: Japans MET ENGELSE TEKST!</b></p>
+      </article>
+    </section>
+
+
+  </main>
+
+  <?php include 'footer.html'?>
+
+
+</body>
+
+</html>
