f8f36d651df3bdbac847898cea595a906f1862ff
Boaz Arsav <9004955@student.tcrmbo.nl>
2021-12-08 14:06:07 +0100

    phonelist push 1

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact page</title>
+
+
+</head>
+
+
+
+
+<body>
+
+    <?php include 'nav.html'?>
+
+
+    <main class="main-contact">
+
+        <section class="con-con">
+            <section class="contact-items">
+                <h1>Contact us!</h1>
+                <P>Phone: +31 88 123 567 875</P>
+                <p>Email: Saturnus@helpdesk.com</p>
+                <p>Fax: 457-878-9056</p>
+                <a class="con-mailto" href="mailto:saturnus@helpdesk.com">Saturnus@helpdesk.com</a>
+                <a class="con-tel" href="tel:+31-88-123-567-875">+31 88 123 567 875</a>
+
+                <p class="loc">Get to know our location!</p>
+
+
+                <section class="conimg">
+
+                    <img class="helpimg" src="images/help.jpg">
+
+
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7376.158606462548!2d4.487792757144728!3d51.90663841316018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5cd3e9b4611e3%3A0xbe1d04c121cc611c!2sMedia%20Markt%20-%20Saturn%20Holding%20Netherlands!5e0!3m2!1sen!2snl!4v1638792683256!5m2!1sen!2snl"
+                         allowfullscreen="" loading="lazy">
+                    </iframe>
+
+                </section>
+
+
+
+            </section>
+
+
+
+        </section>
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+    <?php include 'footer.html'; ?>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    
+</head>
+<body>
+
+<!-- hier include je de navbar -->
+<?php include 'nav.html'?>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<!-- hier include je de Footer -->
+<?php include 'footer.html'?>
+
+</html>
