71a31da5ffc85f84173cda741dcec2ebce7acb41
9017412@student.zadkine.nl <9017412@student.zadkine.nl>
2021-12-20 13:02:12 +0100

    klachten

--- laptops.php
+++ laptops.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Games</title>
+  <title>laptops</title>
   <link id="pagestyle" rel="stylesheet" type="text/css" href="css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
