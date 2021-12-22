b562d499740ff11e6416c34a100642a33d4b0da8
Boaz Arsav <9004955@student.tcrmbo.nl>
2021-12-22 17:48:55 +0100

    Update naar meest recent

--- gamelist.php
+++ gamelist.php
@@ -7,7 +7,7 @@
   <link id="pagestyle" rel="stylesheet" type="text/css" href="css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
 </head>
 
 <body>
@@ -15,8 +15,12 @@
   <?php include 'nav.html'?>
 
   <main class="product-main">
+    <form class="searchbar-mobile" method="post" action="searchbar.php">
+      <input type="text" placeholder="search..." name="search">
+      <button type="submit" value="search">search</button>
+    </form>
 
-  <?php
+    <?php
      // leg verbinding met database
      require_once("dbconnsaturnus.php");
 
@@ -27,16 +31,25 @@
      foreach ($resultq as $data) {
 
       echo "<fieldset></fieldset>";
-
-
+         
          echo "title: " . $data["name"];
          echo "<br>";
          echo "genre: " . $data["genre"];
          echo "<br>";
          echo "released: " . $data["releasedate"];
          echo "<br>";
-         echo "price: " . $data["price"] . "$";
+         echo "price: " . "$" . $data["price"];
          echo "<br>";
+         $name = $data["model"];
+         // Make lower case
+         $url = strtolower($name);
+         //Make alphanumeric
+         $url = preg_replace("/[^a-z0-9\s-]/", "", $url);
+        //Clean up multiple dashes or whitespaces
+        $url = preg_replace("/[\s-]+/", " ", $url);
+        //Convert whitespaces and underscore to dash
+        $url = preg_replace("/[\s]/", "-", $url);
+        echo "<img src=\"images/" . $url . ".jpg\" alt=\"" . $name . "\">";
          echo "<br>";
          echo "<br>";
          
@@ -47,7 +60,7 @@
     ?>
 
 
-   
+
 
   </main>
 
--- laptops.php
+++ laptops.php
@@ -3,11 +3,11 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Games</title>
+  <title>laptops</title>
   <link id="pagestyle" rel="stylesheet" type="text/css" href="css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
 </head>
 
 <body>
@@ -15,10 +15,14 @@
   <?php include 'nav.html'?>
 
   <main class="product-main">
+    <form class="searchbar-mobile" method="post" action="searchbar.php">
+      <input type="text" placeholder="search..." name="search">
+      <button type="submit" value="search">search</button>
+    </form>
 
-  <section class="laptopphp">
+    <section class="laptopphp">
 
-  <?php
+      <?php
      // leg verbinding met database
      require_once("dbconnsaturnus.php");
 
@@ -44,7 +48,18 @@
          echo "<br>";
          echo "disk: " . $data["space"];
          echo "<br>";
-         echo "price : " . $data["price"] . "$";
+         echo "price: " . "$" . $data["price"];
+         echo "<br>";
+         $name = $data["model"];
+         // Make lower case
+         $url = strtolower($name);
+         //Make alphanumeric
+         $url = preg_replace("/[^a-z0-9\s-]/", "", $url);
+        //Clean up multiple dashes or whitespaces
+        $url = preg_replace("/[\s-]+/", " ", $url);
+        //Convert whitespaces and underscore to dash
+        $url = preg_replace("/[\s]/", "-", $url);
+        echo "<img src=\"images/" . $url . ".jpg\" alt=\"" . $name . "\">";
          echo "<br>";
          echo "<br>";
         
@@ -53,11 +68,11 @@
 
     ?>
 
-</section>
+    </section>
 
 
-   
-    
+
+
 
   </main>
 
