0ac452a4b155c10fa3904773086721f2b6a0b906
Jxylia <70641347+Jxylia@users.noreply.github.com>
2022-01-18 14:12:19 +0100

    done

---
+++ overons.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+
+
+</head>
+
+
+
+
+<body>
+
+    <?php include 'nav.html'?>
+
+
+    <main class="product-main">
+        <form class="searchbar-mobile" method="post" action="searchbar.php">
+            <input type="text" placeholder="search..." name="search">
+            <button type="submit" value="search">search</button>
+        </form>
+
+
+        <h1>About us.</h1>
+
+        <br>
+
+        <h2>Our staff.</h2>
+
+        <section class="personel-pictures">
+
+
+        <h3>Boaz</h3>
+        <img src="personel-img/boaz.jpg"> 
+        <br>
+        <h3>Sten</h3>
+        <img src="personel-img/sten.jpg">
+        <br>
+        <h3>Jayjay</h3>
+        <img src="personel-img/jayjay.jpg">
+        <br>
+        <h3>Esra</h3>
+        <img src="personel-img/Ezra.jpg">
+
+
+        </section>
+        <p>
+
+
+
+
+
+
+
+
+
+        </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+    <?php include 'footer.html'; ?>
+
+
+</body>
+
+</html>
